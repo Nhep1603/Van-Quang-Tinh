02710365ad62b892750bf061cbe3981a96f871dc
Nhep Nhep <nhep1603@gmail.com>
2021-11-08 10:51:28 +0700

    set up structure

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+import 'config/routes.dart';
+
+class App extends StatelessWidget {
+  const App({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: buildRoutes(),
+      home: const HomeScreen(),
+    );
+  }
+}
