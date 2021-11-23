7ce5b1798ba665cfcaad2165ccd4a51bcb43e04f
unknown <lehuutinh147@gmail.com>
2021-11-23 15:24:01 +0700

    changed app flie

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'package:van_quang_tinh/src/blocs/crypto_detail/crypto_detail_bloc.dart';
+import 'package:van_quang_tinh/src/services/crypto_currency/crypto_currency_impl.dart';
 
 import './blocs/category/category_bloc.dart';
 import './config/routes.dart';
 import './screens/home_screen.dart';
 import './services/category/app_category_service.dart';
+import 'blocs/search/search_bloc.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -19,6 +21,11 @@ class App extends StatelessWidget {
         BlocProvider(
               create: (context) => CategoryBloc(
                 service: AppCategoryService(httpClient),
+              ),
+            ),
+            BlocProvider(
+              create: (context) => SearchBloc(
+                service: CryptoCurrencyImpl(httpClient),
               ),
             ),
             BlocProvider(
