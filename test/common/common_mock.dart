eb32a85d45aca3671f6fa0427e9404306de59aab
unknown <lehuutinh147@gmail.com>
2021-11-23 14:52:46 +0700

    add testing file

---
+++ test/common/common_mock.dart
@@ -0,0 +1,21 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:van_quang_tinh/src/blocs/category/category_bloc.dart';
+import 'package:van_quang_tinh/src/blocs/category/category_event.dart';
+import 'package:van_quang_tinh/src/blocs/category/category_state.dart';
+import 'package:van_quang_tinh/src/services/category/category_service.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+class MyTypeFake extends Fake implements Route {}
+
+class MockCategoryBloc extends MockBloc<CategoryEvent, CategoryState>
+    implements CategoryBloc {}
+
+class MockCategoryService extends Mock implements CategoryService {}
+
+class FakeCategoryState extends Fake implements CategoryState {}
+
+class FakeCategoryEvent extends Fake implements CategoryEvent {}
--- test/src/screens/search_screen_test.dart
+++ test/src/screens/search_screen_test.dart
@@ -7,7 +7,7 @@ import 'package:van_quang_tinh/src/models/crypto.dart';
 import 'package:van_quang_tinh/src/models/data.dart';
 import 'package:van_quang_tinh/src/screens/search_screen.dart';
 import 'package:mocktail/mocktail.dart';
-import '../mock_data/category_mock_data.dart';
+import '../../mock_data/category_mock_data.dart';
 import '../../src/common/common_mock.dart';
 
 void main() {
