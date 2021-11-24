dad3c8b0b887da83bf434084765cf751b5662281
vanquangpham165 <90913961+vanquangpham165@users.noreply.github.com>
2021-11-24 08:58:32 +0700

    bloc test and test widget coin search

--- test/src/widgets/coin_search_test.dart
+++ test/src/widgets/coin_search_test.dart
@@ -6,17 +6,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import 'package:van_quang_tinh/src/blocs/crypto_detail/crypto_detail_bloc.dart';
+import 'package:van_quang_tinh/src/blocs/crypto_detail/crypto_detail_state.dart';
 import 'package:van_quang_tinh/src/blocs/search/search_bloc.dart';
 import 'package:van_quang_tinh/src/blocs/search/search_state.dart';
 import 'package:van_quang_tinh/src/config/routes.dart';
 import 'package:van_quang_tinh/src/models/crypto.dart';
 import 'package:van_quang_tinh/src/screens/search_screen.dart';
 import 'package:van_quang_tinh/src/services/crypto_currency/crypto_currency_service.dart';
+import 'package:van_quang_tinh/src/widgets/coin_search.dart';
 import '../../mock_data/crypto_mock_data.dart';
 import '../../common/common_mock.dart';
 
 main() {
   final mockResponse = json.decode(mockCryptoData);
+  final mockObserver = MockNavigatorObserver();
   
   setUpAll(() {
     registerFallbackValue(FakeSearchState());
@@ -118,4 +121,85 @@ main() {
               .icon;
       expect(textFieldFinder, Icons.search);
     });
+
+        testWidgets(
+        'Should render Category\'s data in TextField when bloc state is [SearchLoadSucess]',
+        (tester) async {
+      when(() => searchBloc.state).thenReturn(SearchLoadSucess(
+          cryptos: List<Crypto>.from(
+              mockResponse.map((model) => Crypto.fromJson(model)))));
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+      final textFieldFinder = find.byType(TextField);
+      expect(textFieldFinder, findsOneWidget);
+    });
+
+    testWidgets(
+        'Display AutoComplete widget when bloc state is [SearchLoadSucess]',
+        (tester) async {
+      when(() => searchBloc.state).thenReturn(SearchLoadSucess(
+          cryptos: List<Crypto>.from(
+              mockResponse.map((model) => Crypto.fromJson(model)))));
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+      final autocompleteFinder = find
+          .byType(Autocomplete<Crypto>(optionsBuilder: (_) => []).runtimeType);
+      expect(autocompleteFinder, findsOneWidget);
+    });
+
+    testWidgets(
+        'Should render red container with error message when search bloc state is [SearchLoadFailure]',
+        (tester) async {
+      when(() => searchBloc.state)
+          .thenReturn(SearchLoadFailure(errorMessage: 'errorMessage'));
+      await tester.pumpWidget(widget);
+      await tester.pump();
+      final errorMessageFinder = find.text('errorMessage');
+      expect(errorMessageFinder, findsOneWidget);
+      expect((tester.widget(find.byType(Container)) as Container).color,
+          Colors.red);
+    });
+
+    testWidgets(
+        'Should render orange container with error message when not have any bloc state',
+        (tester) async {
+      when(() => searchBloc.state).thenReturn(SearchInitial());
+      await tester.pumpWidget(widget);
+      await tester.pump();
+      expect((tester.widget(find.byType(Container)) as Container).color,
+          Colors.orange);
+    });
+
+    testWidgets(
+        'Should render Search bar when bloc state is [SearchLoadSuccess]',
+        (tester) async {
+      when(() => searchBloc.state).thenReturn(SearchLoadSucess(
+        cryptos: List<Crypto>.from(
+            mockResponse.map((model) => Crypto.fromJson(model))).toList(),
+      ));
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+      expect(find.byType(SearchScreen), findsOneWidget);
+    });
+
+    testWidgets('Should search Coin when bloc state is [SearchLoadSuccess]',
+        (tester) async {
+      when(() => cryptoDetailBloc.state).thenReturn(CryptoDetailInitial());
+      when(() => searchBloc.state).thenReturn(SearchLoadSucess(
+        cryptos: List<Crypto>.from(
+            mockResponse.map((model) => Crypto.fromJson(model))).toList(),
+      ));
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(CoinSearch));
+      await tester.enterText(find.byType(CoinSearch), 'bit');
+      await tester.pump(const Duration(seconds: 1));
+      final coinCardFinder = find.descendant(
+          of: find.byType(ListView), matching: find.byType(Card).first);
+      expect(coinCardFinder, findsOneWidget);
+      await tester.tap(coinCardFinder);
+      await tester.pumpAndSettle();
+      verifyNever(() => mockObserver.didPush(any(), any()));
+    });
+    
 }
