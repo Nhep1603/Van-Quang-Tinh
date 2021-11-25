b85bf757389e0e5630d7ba6c199885184049356d
unknown <lehuutinh147@gmail.com>
2021-11-25 08:26:33 +0700

    fix app import

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,9 +6,9 @@ import './blocs/crypto_detail/crypto_detail_bloc.dart';
 import './blocs/category/category_bloc.dart';
 import './blocs/search/search_bloc.dart';
 import './config/routes.dart';
+import './services/category/impl_category_service.dart';
 import './services/crypto_currency/crypto_currency_impl.dart';
 import './screens/home_screen.dart';
-import 'services/category/impl_category_service.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
